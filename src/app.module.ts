b2395a2cd5c497fdd575e23e1ac7cf1d96741b8f
Ionatan Dumea <134598668+ionatandumea@users.noreply.github.com>
2024-12-30 23:54:59 +0200

    chore: config setup and env validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ExercisesModule } from './exercises/exercises.module';
-
+import { ConfigModule } from '@nestjs/config';
+import { validate } from './env.validation';
 @Module({
-  imports: [ExercisesModule],
+  imports: [
+    ExercisesModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/exercises/dto/create-exercise.dto.ts
+++ src/exercises/dto/create-exercise.dto.ts
@@ -1,1 +1,15 @@
-export class CreateExerciseDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateExerciseDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @IsString()
+  @IsNotEmpty()
+  difficulty: string;
+}
